5eaa26178aec9eebf363ed6d41a0d67fda34f44e
Wandrey Warley <wandrey09@gmail.com>
2021-03-11 16:54:29 -0300

    scroll suave transformado em class

--- js/modules/scroll-suave.js
+++ js/modules/scroll-suave.js
@@ -1,24 +1,34 @@
 // Essa função cria um scroll suave ao clicar nos links do cabeçalho
-export default function initScroolSuave() {
-  const linksInternos = document.querySelectorAll('[data-menu="suave"] a[href^="#"]');
+export default class ScrollSuave {
+  constructor(links, options) {
+    this.linksInternos = document.querySelectorAll(links);
 
-  function scrollToSection(event) {
+    if (options === undefined) {
+      this.options = { behavior: 'smooth', block: 'start' };
+    } else {
+      this.options = options;
+    }
+
+    this.scrollToSection = this.scrollToSection.bind(this);
+  }
+
+  scrollToSection(event) {
     event.preventDefault();
     const href = event.currentTarget.getAttribute('href');
     const section = document.querySelector(href);
-
-    section.scrollIntoView({
-      behavior: 'smooth',
-      block: 'start',
-    });
-    // Forma alternativa
-    // const topo = section.offsetTop;
-    // window.scrollTo({
-    // top: topo,
-    // behavior: 'smooth',
-    // });
+    section.scrollIntoView(this.options);
   }
-  linksInternos.forEach((link) => {
-    link.addEventListener('click', scrollToSection);
-  });
+
+  addLinkEvent() {
+    this.linksInternos.forEach((link) => {
+      link.addEventListener('click', this.scrollToSection);
+    });
+  }
+
+  init() {
+    if (this.linksInternos.length) {
+      this.addLinkEvent();
+    }
+    return this;
+  }
 }
--- js/script.js
+++ js/script.js
@@ -1,5 +1,5 @@
 // Script principal que importa todos os modulos e os inicializa
-import initScroolSuave from './modules/scroll-suave.js';
+import ScrollSuave from './modules/scroll-suave.js';
 import initScrollAnimated from './modules/scroll-animation.js';
 import initAccordion from './modules/accordion.js';
 import initTabNav from './modules/tabNav.js';
@@ -11,18 +11,16 @@ import initFuncionamento from './modules/funcionamento.js';
 import initFetchAnimais from './modules/fetchAnimais.js';
 import initFetchBtc from './modules/fetchBtc.js';
 
-window.onload = () => {
-  initScroolSuave();
-  initScrollAnimated();
-  initAccordion();
-  initTabNav();
-  initModal();
-  initTooltip();
-  initDropDownMenu();
-  initMenuMobile();
-  initFuncionamento();
-  initFetchAnimais();
-  initFetchBtc();
-};
+const scrollSuave = new ScrollSuave('[data-menu="suave"] a[href^="#"]');
+scrollSuave.init();
 
-console.log('Esta funcionando Obaaaaaaaaaaaaaa');
+initScrollAnimated();
+initAccordion();
+initTabNav();
+initModal();
+initTooltip();
+initDropDownMenu();
+initMenuMobile();
+initFuncionamento();
+initFetchAnimais();
+initFetchBtc();
